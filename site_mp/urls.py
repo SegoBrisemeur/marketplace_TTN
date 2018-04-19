b274cef2b08cf0898ff7ef12b534d4953c733847
segolene <sego.b@hotmail.fr>
2018-04-19 11:27:15 +0200

    Addition of viewsto create and edit a thing

--- site_mp/urls.py
+++ site_mp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     	url(r'^$', views.thing_list, name='thing_list'),
-	url(r'^post/(?P<pk>[0-9]+)/$', views.thing_detail, name='thing_detail'),
+	url(r'^thing/(?P<pk>[0-9]+)/$', views.thing_detail, name='thing_detail'),
+    	url(r'^thing/new/$', views.thing_new, name='thing_new'),
+        url(r'^thing/(?P<pk>[0-9]+)/edit/$', views.thing_edit, name='thing_edit'),
+
+
 ]
--- site_mp/views.py
+++ site_mp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Thing
 from django.shortcuts import render, get_object_or_404
+from .forms import ThingForm
+from django.shortcuts import redirect
+
 
 
 def thing_list(request):
@@ -12,3 +15,29 @@ def thing_list(request):
 def thing_detail(request, pk):
     thing = get_object_or_404(Thing, pk=pk)
     return render(request, 'site_mp/thing_detail.html', {'thing': thing})
+
+def thing_new(request):
+    if request.method == "POST":
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            thing = form.save(commit=False)
+            thing.created_date = timezone.now()
+            thing.edit_date = timezone.now()
+            thing.save()
+            return redirect('thing_detail', pk=thing.pk)
+    else:
+        form = ThingForm()
+    return render(request, 'site_mp/thing_edit.html', {'form': form})
+
+def thing_edit(request, pk):
+    thing = get_object_or_404(Thing, pk=pk)
+    if request.method == "POST":
+        form = ThingForm(request.POST, instance=thing)
+        if form.is_valid():
+            thing = form.save(commit=False)
+            thing.edit_date = timezone.now()
+            thing.save()
+            return redirect('thing_detail', pk=thing.pk)
+    else:
+        form = ThingForm(instance=thing)
+    return render(request, 'site_mp/thing_edit.html', {'form': form})
